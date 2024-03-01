85cf8ce081a13b36332b100f8d07ea3d0e717f03
Eyerusalem Kidane <140943843+Ola-je@users.noreply.github.com>
2024-03-01 08:22:47 +0300

    Add solution for Partition List

---
+++ a2sv/week1/leetcode/partition-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        dummy1 = ListNode()
+        dummy2 = ListNode()
+        small = dummy1
+        large = dummy2
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                large.next = head
+                large = large.next
+            head = head.next
+        small.next = dummy2.next
+        large.next = None
+        return dummy1.next
