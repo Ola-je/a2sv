7bb639ec1771ea2491e60155c319233d38607dcd
Eyerusalem Kidane <140943843+Ola-je@users.noreply.github.com>
2024-03-03 15:16:49 +0300

    Add solution for Count Pairs Whose Sum is Less than Target

---
+++ a2sv/week1/leetcode/count-pairs-whose-sum-is-less-than-target.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        for i in range(len(nums)-1):
+            j = i +1
+            while j < len(nums):
+                if nums[i] + nums[j] < target:
+                    count += 1
+                j += 1
+        return count
+            
